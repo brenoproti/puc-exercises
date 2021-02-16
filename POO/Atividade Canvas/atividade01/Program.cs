7a56a531bf102709531a384ca27673f43529a2de
BrenoProti <moreirasgbr@gmail.com>
2021-02-16 11:10:30 -0300

    atv 01 Canvas

---
+++ POO/Atividade Canvas/atividade01/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace atividade01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variaveis
+            int horas = 190;
+            double salario = 12.00, salarioFinal;
+            //Criar um objeto do tipo Empregado
+            Empregado fulano = new Empregado();
+
+            //Definir o valor salario-hora e horas trabalhadas
+            fulano.setSalarioHora(salario);
+            fulano.setHrsTrabalhadas(horas);
+
+            //Calcular o salario final 
+            salarioFinal = fulano.calcularSalario();
+
+            //Exibir o resultado na tela
+            Console.WriteLine("Salario-hora: R$ {0:F} \nHoras trabalhadas: {1}\nSalario final: R$ {2:F}", fulano.getSalarioHora(), fulano.getHrsTrabalhadas(), salarioFinal);
+        }
+    }
+
+    class Empregado {
+        private double salarioHora;
+        private int hrsTrabalhadas;
+
+        //Constructors
+        public Empregado() { 
+            setSalario(0,0);
+        }
+
+        public Empregado(double salario) { 
+            setSalarioHora(salario);
+        } 
+
+        public Empregado(int horas) { 
+            setHrsTrabalhadas(horas);
+        } 
+
+        //GET and SET 
+        public double getSalarioHora() {
+            return salarioHora;
+        }
+
+        public double getHrsTrabalhadas() {
+            return hrsTrabalhadas;
+        }
+
+        public void  setSalarioHora(double salario) {
+            salarioHora = salario;
+        }
+
+        public void setHrsTrabalhadas(int horas) {
+            hrsTrabalhadas = horas;
+        }
+
+        public void setSalario(double salario, int hora) {
+            setSalarioHora(salario);
+            setHrsTrabalhadas(hora);
+        }
+
+        //Methods
+
+        public double calcularSalario() {
+            double INSS;
+            double salarioFinal;
+            
+            //Calcular o salario final
+            salarioFinal = salarioHora * hrsTrabalhadas;
+            INSS = (salarioFinal *  11) / 100;
+
+            salarioFinal -= INSS;
+
+            return salarioFinal;
+        }
+        
+    }
+}
